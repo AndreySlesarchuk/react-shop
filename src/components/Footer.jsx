51c9c85ccd1a62407a811a36ab60027672fe90f6
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-10-10 17:50:36 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 function Footer() {
-    return <footer className="page-footer green lighten-4">
-        <div className="footer-copyright">
-            <div className="container">
+    return <footer className='page-footer cyan lighten-3'>
+        <div className='footer-copyright'>
+            <div className='container'>
                 © {new Date().getFullYear()} Copyright ATMM
-                <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                <a className='grey-text text-lighten-4 right'
+                   href='https://github.com/AndreySlesarchuk/react-shop'
+                   target='_blank'
+                   rel='noreferrer'
+                >Repo</a>
             </div>
         </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 function Header() {
-    return <nav className="green darken-1">
-        <div className="nav-wrapper">
-            <a href="!#" className="brand-logo">React shop</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
+    return <nav className='cyan darken-1'>
+        <div className='nav-wrapper'>
+            <a href='/' className='brand-logo'>React shop</a>
+            <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                <li>
+                    <a
+                        href='https://github.com/AndreySlesarchuk/react-shop'
+                        target='_blank'
+                        rel='noreferrer'
+                    >Repo</a>
+                </li>
             </ul>
         </div>
     </nav>
